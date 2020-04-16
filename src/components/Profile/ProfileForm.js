cd3cc6b703433b9b08b3a1351180d716634f36d2
aurelieverrot <aurelie.verrot@gmail.com>
2020-04-16 16:22:14 -0700

    profile form gets user info as default value

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,19 +1,19 @@
-// Form inside a modal to update the profile.
-
 import React from 'react';
 import { Form } from 'semantic-ui-react';
+import './Profile.css'
 
 const ProfileForm = (props) => {
 
 
 
   return (
-    <Form className="column">
-      <p>this is the ProfileForm</p>
-      <Form.Group>
-        <Form.Input name="username" fluid label='Username' placeholder={props.user.username}/>
-        <Form.Input name="username" fluid label='Email' placeholder={props.user.email}/>
-        <Form.Input/>
+    <Form className="">
+      <h2>Your information</h2>
+      <Form.Group className="form-column" widths='equal'>
+        
+        <Form.Input name="username" fluid label='Your Username' defaultValue={props.user.username}/>
+        <Form.Input name="email" fluid label='Your Email' defaultValue={props.user.email}/>
+        
       </Form.Group>
     </Form>
   )
